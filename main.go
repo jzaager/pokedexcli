9eb71c7cc9242f8278da1e7eed9e80c93d2eb625
Justin <94499968+jzaager@users.noreply.github.com>
2025-01-02 20:56:10 -0500

    Add main repl loop

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	fmt.Println("Hello, World!")
-}
-
-func cleanInput(text string) []string {
-	if len(text) == 0 {
-		return []string{}
-	}
-	clean := strings.TrimSpace(text)
-	lower := strings.ToLower(clean)
-	words := strings.Split(lower, " ")
-	return words
+	startRepl()
 }
